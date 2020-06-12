8a8115c99212f5a836848e60f3e03707b377e522
GerritV <shigw5420@joyowo.com>
2020-06-12 15:54:56 +0800

    按类型重组计算过程，使用多态计算器来提供数据

--- src/statement/create-statement.js
+++ src/statement/create-statement.js
@@ -10,9 +10,13 @@ export function createStatementData(invoice, plays) {
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+    const calculator = createPerformanceCalculator(
+      aPerformance,
+      playFor(result)
+    );
+    result.play = calculator.play;
+    result.amount = calculator.amount;
+    result.volumeCredits = calculator.volumeCredits;
 
     return result;
 
@@ -21,36 +25,13 @@ export function createStatementData(invoice, plays) {
     }
 
     function amountFor(aPerformance) {
-      let result = 0;
-      switch (playFor(aPerformance).type) {
-        case "tragedy":
-          result = 40000;
-          if (aPerformance.audience > 30) {
-            result += 1000 * (aPerformance.audience - 30);
-          }
-          break;
-        case "comedy":
-          result = 30000;
-          if (aPerformance.audience > 20) {
-            result += 10000 + 500 * (aPerformance.audience - 20);
-          }
-          result += 300 * aPerformance.audience;
-          break;
-        default:
-          throw new Error(`unknown type: ${aPerformance.play.type}`);
-      }
-
-      return result;
+      return new PerformanceCalculator(aPerformance, playFor(aPerformance))
+        .amount;
     }
 
     function volumeCreditsFor(aPerformance) {
-      let result = 0;
-      // add volume credits
-      result += Math.max(aPerformance.audience - 30, 0);
-      // add extra credit for every ten comedy attendees
-      if ("comedy" === aPerformance.play.type)
-        result += Math.floor(aPerformance.audience / 5);
-      return result;
+      return new PerformanceCalculator(aPerformance, playFor(aPerformance))
+        .volumeCredits;
     }
   }
 
@@ -63,5 +44,61 @@ export function createStatementData(invoice, plays) {
       (prev, curr) => prev + curr.volumeCredits,
       0
     );
+  }
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`unknown type: ${aPlay.type}`);
+  }
+}
+
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+
+  get amount() {
+    throw new Error("subclass responsibility");
+  }
+
+  get volumeCredits() {
+    return Math.max(this.performance.audience - 30, 0);
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 0;
+
+    result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 0;
+
+    result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+
+    return result;
   }
 }
